b1f22dbcd92e35190e13cf5fbe8a6e96aac356db
zipp86 <solomonsremodeling9@gmail.com>
2024-06-26 09:24:03 -0500

    Updates

---
+++ precache-manifest.1f6fcb257fd19a33518161b068e3cf80.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c3fc69d10930b860a8f9899f648d59a",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "69d50daecf3bb053fef7",
+    "url": "/myapp/static/js/2.00337174.chunk.js"
+  },
+  {
+    "revision": "a51173b482590d000a18",
+    "url": "/myapp/static/js/main.10525878.chunk.js"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  }
+]);
